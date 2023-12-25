b09bd64588815d91a7436acf89a7c33f3dd8ebb0
awd0786 <himanshu07.sharma@yahoo.com>
2023-12-25 11:22:15 +0530

    Add project files.

---
+++ Backend Project/Controllers/EmployeeController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Backend_Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public EmployeeController (IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult GetEmployee()
+        {
+            string query = @"Exec SP_EmployeeList_BackendProject";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("BackendProjectConnection");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+
+        //[HttpGet]
+        //public JsonResult GetEmployee(string action = null)
+        //{
+        //    string query = "EXEC SP_EmployeeList_BackendProject @Action";
+        //    DataTable table = new DataTable();
+        //    string sqlDataSource = _configuration.GetConnectionString("BackendProjectConnection");
+
+        //    using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+        //    {
+        //        myCon.Open();
+
+        //        using (SqlCommand myCommand = new SqlCommand(query, myCon))
+        //        {
+        //            myCommand.Parameters.Add("@Action", SqlDbType.NVarChar).Value = (object)action ?? DBNull.Value;
+
+        //            using (SqlDataReader myReader = myCommand.ExecuteReader())
+        //            {
+        //                table.Load(myReader);
+        //            }
+        //        }
+        //    }
+
+        //    return new JsonResult(table);
+        //}
+
+
+    }
+}
---
+++ Backend Project/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿namespace Backend_Project.Models
+{
+    public class Employee
+    {
+        //public int id { get; set; }
+        //public string EmployeeId { get; set; }
+        //public string FullName { get; set; }
+
+        //public DateTime DOB { get; set; }
+        //public int Age { get; set; }
+        //public string Address { get; set; }
+
+        //public string MobileNo { get; set; }
+
+        //public string DeptName { get; set; }
+
+        //public  string Speciality { get; set; }
+
+
+
+    }
+}
